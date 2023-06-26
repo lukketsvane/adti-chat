efbf848f860afbb98737911a79a89b706a4f290f
unknown <iverfinne@gmail.com>
2023-06-26 08:47:56 +0200

    minor fix

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -19,3 +19,31 @@ export const getServerSideProps: GetServerSideProps<DocsIndexProps> = async () =
   const docs = getAllDocs();
   return { props: { docs } };
 };
+./pages/docs/[...slug].tsx:
+
+jsx
+Copy code
+import { GetServerSidePropsContext, GetServerSideProps } from 'next';
+import { getAllDocs, getDocBySlug, Doc } from '@/lib/docs';
+import { DocPage } from '@/components/DocPage';
+import Layout from '@/components/layout';
+
+type DocsProps = {
+  selectedDoc: Doc;
+  docs: Doc[];
+};
+
+export default function Docs({ selectedDoc, docs }: DocsProps) {
+  return (
+    <Layout>
+      <DocPage docs={docs} selectedDoc={selectedDoc} />
+    </Layout>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<DocsProps> = async ({ params }: GetServerSidePropsContext) => {
+  const slug = params?.slug as string[];
+  const selectedDoc = await getDocBySlug(slug);
+  const docs = await getAllDocs();
+  return { props: { selectedDoc, docs } };
+};
