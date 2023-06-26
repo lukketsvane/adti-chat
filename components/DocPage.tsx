aaf18dd94ca51562a88f50d5fd0aeba9e413db31
lukketsvane <iverfinne@gmail.com>
2023-06-26 18:05:44 +0200

    hyphen

--- components/DocPage.tsx
+++ components/DocPage.tsx
@@ -1,105 +1,66 @@
-// components/DocPage.tsx
-import { useState } from 'react';
-import { Doc } from '@/lib/docs';
-import Link from 'next/link';
-import ReactMarkdown from 'react-markdown';
-import gfm from 'remark-gfm';
-import markdownComponents from './markdownComponents';
-
-type DocPageProps = {
-  docs: Doc[];
-  selectedDoc: Doc;
-};
-
-export function DocPage({ docs, selectedDoc }: DocPageProps) {
-  const [expandedFolders, setExpandedFolders] = useState<string[]>([]);
-
-  const renderDocLink = (doc: Doc, folder: string) => {
-    const isSelected = doc.filePath === selectedDoc.filePath;
-    const displayTitle = removeNumberPrefix(doc.data.title || doc.filePath.split('/').pop());
-
-    return (
-      <li
-        key={doc.filePath}
-        className={`${
-          isSelected ? 'font-semibold text-gray-800' : 'text-gray-500'
-        } user-select-none`}
-      >
-        <Link href={doc.filePath.replace(/ /g, '-')} passHref>
-          <span className={isSelected ? 'font-bold text-gray-800' : ''}>
-            {displayTitle}
-          </span>
-        </Link>
-      </li>
-    );
-  };
-
-  const docsByFolder: Record<string, Doc[]> = docs.reduce((acc, doc) => {
-    const parts = doc.filePath.split('/');
-    const folder = parts.slice(0, -1).join('/');
-    acc[folder] = [...(acc[folder] || []), doc];
-    return acc;
-  }, {});
-
-  const handleFolderClick = (folder: string) => {
-    const newExpandedFolders = [...expandedFolders];
-    const index = newExpandedFolders.indexOf(folder);
-    if (index === -1) {
-      newExpandedFolders.push(folder);
-    } else {
-      newExpandedFolders.splice(index, 1);
-    }
-    setExpandedFolders(newExpandedFolders);
-  };
-
-  const removeNumberPrefix = (title: string): string => {
-    return title.replace(/^\d+(\.\d+)?(-)? /, '');
-  };
-
-  const sortedFolders = Object.keys(docsByFolder).sort();
+const renderDocLink = (doc: Doc, folder: string) => {
+  const isSelected = doc.filePath === selectedDoc.filePath;
+  const displayTitle = removeNumberPrefix(doc.data.title || doc.filePath.split('/').pop());
 
   return (
-    <div className="flex h-screen overflow-hidden">
-      <nav className="w-64 bg-white border-r dark:bg-gray-800 dark:border-gray-600 overflow-auto px-4">
-        {sortedFolders.map((folder) => {
-          const folderTitle = folder.split('/').pop();
-          const displayFolderTitle = removeNumberPrefix(folderTitle);
-          return (
-            <div
-              key={folder}
-              className="transition-all duration-500 user-select-none"
-            >
-              <div
-                className={`${
-                  selectedDoc.filePath.includes(folder)
-                    ? 'font-semibold text-gray-800 cursor-pointer'
-                    : 'text-gray-500 cursor-pointer'
-                }`}
-                onClick={() => handleFolderClick(folder)}
-              >
-                {displayFolderTitle}
-              </div>
-              {expandedFolders.includes(folder) && (
-                <ul className="space-y-2 pl-2">
-                  {docsByFolder[folder].map((doc) =>
-                    renderDocLink(doc, folder)
-                  )}
-                </ul>
-              )}
-            </div>
-          );
-        })}
-      </nav>
-      <main className="flex-1 p-10 overflow-auto">
-        <div className="prose dark:prose-dark max-w-none overflow-scroll">
-          <ReactMarkdown
-            components={markdownComponents}
-            remarkPlugins={[gfm]}
-          >
-            {selectedDoc.content}
-          </ReactMarkdown>
-        </div>
-      </main>
-    </div>
+    <li
+      key={doc.filePath}
+      className={`${
+        isSelected ? 'font-semibold text-gray-800' : 'text-gray-500'
+      } user-select-none`}
+    >
+      <Link href={doc.filePath.replace(/ /g, '-')} passHref>
+        <span className={isSelected ? 'font-bold text-gray-800' : ''}>
+          {displayTitle}
+        </span>
+      </Link>
+    </li>
   );
-}
+};
+
+const sortedFolders = Object.keys(docsByFolder).sort();
+
+return (
+  <div className="flex h-screen overflow-hidden">
+    <nav className="w-64 bg-white border-r dark:bg-gray-800 dark:border-gray-600 overflow-auto px-4">
+      {sortedFolders.map((folder) => {
+        const folderTitle = folder.split('/').pop();
+        const displayFolderTitle = removeNumberPrefix(folderTitle);
+        return (
+          <div
+            key={folder}
+            className="transition-all duration-500 user-select-none"
+          >
+            <div
+              className={`${
+                selectedDoc.filePath.includes(folder)
+                  ? 'font-semibold text-gray-800 cursor-pointer'
+                  : 'text-gray-500 cursor-pointer'
+              }`}
+              onClick={() => handleFolderClick(folder)}
+            >
+              {displayFolderTitle}
+            </div>
+            {expandedFolders.includes(folder) && (
+              <ul className="space-y-2 pl-2">
+                {docsByFolder[folder].map((doc) =>
+                  renderDocLink(doc, folder)
+                )}
+              </ul>
+            )}
+          </div>
+        );
+      })}
+    </nav>
+    <main className="flex-1 p-10 overflow-auto">
+      <div className="prose dark:prose-dark max-w-none overflow-scroll">
+        <ReactMarkdown
+          components={markdownComponents}
+          remarkPlugins={[gfm]}
+        >
+          {selectedDoc.content}
+        </ReactMarkdown>
+      </div>
+    </main>
+  </div>
+);
