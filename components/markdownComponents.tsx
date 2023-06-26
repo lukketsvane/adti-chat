fc98c8094cbe421c5330e2d6183862a8406bc999
unknown <iverfinne@gmail.com>
2023-06-26 07:41:18 +0200

    minor fix

--- components/markdownComponents.tsx
+++ components/markdownComponents.tsx
@@ -1,4 +1,5 @@
 import { Prism as SyntaxHighlighter } from 'prism-react-renderer';
+import Image from 'next/image';
 
 const components = {
   code({ node, inline, className, children, ...props }) {
@@ -11,27 +12,25 @@ const components = {
       </code>
     )
   },
-  h1: ({ node, ...props }) => <h1 style={{ fontSize: "2em", borderBottom: '1px solid #eaecef', paddingBottom: '0.3em', paddingTop: '0.8em' }} {...props} />,
-  h2: ({ node, ...props }) => <h2 style={{ fontSize: "1.5em", borderBottom: '1px solid #eaecef', paddingBottom: '0.3em', paddingTop: '0.8em'  }} {...props} />,
-  h3: ({ node, ...props }) => <h3 style={{ fontSize: "1.2em", paddingTop: '0.8em', paddingBottom: '0.3em'  }} {...props} />,
-  ul: ({ node, ...props }) => <ul style={{ listStyleType: "disc", paddingLeft: '2em', paddingTop: '0.3em', paddingBottom: '0.3em'  }} {...props} />,
-  ol: ({ node, ...props }) => <ol style={{ listStyleType: "decimal", paddingLeft: '2em' , paddingTop: '0.3em', paddingBottom: '0.3em'   }} {...props} />,
-  li: ({ node, ...props }) => <li {...props} />,
-  em: ({ node, ...props }) => <em {...props} />,
-  strong: ({ node, ...props }) => <strong {...props} />,
-  del: ({ node, ...props }) => <del {...props} />,
-  a: ({ node, ...props }) => <a style={{ color: "grey", textDecoration: "underline" }} {...props} />,
-  table: ({ node, ...props }) => <table style={{ borderCollapse: "collapse", width: "100%" }} {...props} />,
-  thead: ({ node, ...props }) => <thead {...props} />,
-  tbody: ({ node, ...props }) => <tbody {...props} />,
-  tr: ({ node, ...props }) => <tr {...props} />,
-  th: ({ node, ...props }) => <th style={{ border: "1px solid #dfe2e5", padding: "6px 13px" }} {...props} />,
-  td: ({ node, ...props }) => <td style={{ border: "1px solid #dfe2e5", padding: "6px 13px" }} {...props} />,
+  h1: (props) => <h1 style={{ fontSize: "2em", borderBottom: '1px solid #eaecef', paddingBottom: '0.3em', paddingTop: '0.8em' }} {...props} />,
+  h2: (props) => <h2 style={{ fontSize: "1.5em", borderBottom: '1px solid #eaecef', paddingBottom: '0.3em', paddingTop: '0.8em'  }} {...props} />,
+  h3: (props) => <h3 style={{ fontSize: "1.2em", paddingTop: '0.8em', paddingBottom: '0.3em'  }} {...props} />,
+  ul: (props) => <ul style={{ listStyleType: "disc", paddingLeft: '2em', paddingTop: '0.3em', paddingBottom: '0.3em'  }} {...props} />,
+  ol: (props) => <ol style={{ listStyleType: "decimal", paddingLeft: '2em' , paddingTop: '0.3em', paddingBottom: '0.3em'   }} {...props} />,
+  li: (props) => <li {...props} />,
+  em: (props) => <em {...props} />,
+  strong: (props) => <strong {...props} />,
+  del: (props) => <del {...props} />,
+  a: (props) => <a style={{ color: "grey", textDecoration: "underline" }} {...props} />,
+  table: (props) => <table style={{ borderCollapse: "collapse", width: "100%" }} {...props} />,
+  thead: (props) => <thead {...props} />,
+  tbody: (props) => <tbody {...props} />,
+  tr: (props) => <tr {...props} />,
+  th: (props) => <th style={{ border: "1px solid #dfe2e5", padding: "6px 13px" }} {...props} />,
+  td: (props) => <td style={{ border: "1px solid #dfe2e5", padding: "6px 13px" }} {...props} />,
   blockquote: ({ children }) => <blockquote style={{ color: '#6a737d', borderLeft: '0.25em solid #dfe2e5', padding: '0 1em' }}>{children}</blockquote>,
   hr: () => <hr />,
-  img: ({className, ...props}) => <img className={`markdown-img ${className}`} {...props} />
-
-
+  img: (props) => <Image className={`markdown-img ${props.className}`} {...props} />
 }
 
 export default components;
