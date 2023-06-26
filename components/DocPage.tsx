c564e3f9972e5bdb79918c4e7ebe08726a6967ac
lukketsvane <iverfinne@gmail.com>
2023-06-26 14:46:11 +0200

    yesh

--- components/DocPage.tsx
+++ components/DocPage.tsx
@@ -53,101 +53,8 @@ export function DocPage({ docs, selectedDoc }: DocPageProps) {
     setExpandedFolders(newExpandedFolders);
   };
 
-  return (
-    <div className="flex h-screen overflow-hidden">
-      <nav className="w-64 bg-white border-r dark:bg-gray-800 dark:border-gray-600 overflow-auto px-4">
-        {Object.keys(docsByFolder).map((folder) => {
-          const folderTitle = folder.split('/').pop();
-          return (
-            <div
-              key={folder}
-              className="transition-all duration-500 user-select-none"
-            >
-              <div
-                className={`${
-                  selectedDoc.filePath.includes(folder)
-                    ? 'font-semibold text-gray-800 cursor-pointer'
-                    : 'text-gray-500 cursor-pointer'
-                }`}
-                onClick={() => handleFolderClick(folder)}
-              >
-                {folderTitle}
-              </div>
-              {expandedFolders.includes(folder) && (
-                <ul className="space-y-2 pl-2">
-                  {docsByFolder[folder].map((doc) => renderDocLink(doc, folder))}
-                </ul>
-              )}
-            </div>
-          );
-        })}
-      </nav>
-      <main className="flex-1 p-10 overflow-auto">
-        <div className="prose dark:prose-dark max-w-none overflow-scroll">
-          <ReactMarkdown
-            components={markdownComponents}
-            remarkPlugins={[gfm]}
-          >
-            {selectedDoc.content}
-          </ReactMarkdown>
-        </div>
-      </main>
-    </div>
-  );
-}
-import { useState } from 'react';
-import { Doc } from '@/lib/docs';
-import Link from 'next/link';
-import ReactMarkdown from 'react-markdown';
-import gfm from 'remark-gfm';
-import markdownComponents from './markdownComponents';
-
-type DocPageProps = {
-  docs: Doc[];
-  selectedDoc: Doc;
-};
-
-export function DocPage({ docs, selectedDoc }: DocPageProps) {
-  const [expandedFolders, setExpandedFolders] = useState<string[]>([]);
-
-  const renderDocLink = (doc: Doc, folder: string) => {
-    const isSelected = doc.filePath === selectedDoc.filePath;
-    const file = doc.filePath.split('/').pop();
-    const displayTitle = file ? file.replace(/^\d+\.\d+ /, '') : '';
-
-    return (
-      <li
-        key={doc.filePath}
-        className={`${
-          isSelected ? 'font-semibold text-gray-800' : 'text-gray-500'
-        } user-select-none`}
-      >
-        <Link href={doc.filePath} passHref>
-          <span className={isSelected ? 'font-bold text-gray-800' : ''}>
-            {displayTitle}
-          </span>
-        </Link>
-      </li>
-    );
-  };
-
-  const docsByFolder: Record<string, Doc[]> = docs.reduce((acc, doc) => {
-    const parts = doc.filePath.split('/');
-    const file = parts.pop();
-    const folder = parts.join('/');
-    acc[folder] = [...(acc[folder] || []), doc];
-    return acc;
-  }, {});
-
-  const handleFolderClick = (folder: string) => {
-    const newExpandedFolders = [...expandedFolders];
-    const index = newExpandedFolders.indexOf(folder);
-    if (index === -1) {
-      newExpandedFolders.push(folder);
-    } else {
-      newExpandedFolders.splice(index, 1);
-    }
-    setExpandedFolders(newExpandedFolders);
+  const hideNumberPrefix = (title: string): string => {
+    return title.replace(/^\d+\.\d+ /, '');
   };
 
   return (
@@ -168,11 +75,13 @@ export function DocPage({ docs, selectedDoc }: DocPageProps) {
                 }`}
                 onClick={() => handleFolderClick(folder)}
               >
-                {folderTitle}
+                {hideNumberPrefix(folderTitle)}
               </div>
               {expandedFolders.includes(folder) && (
                 <ul className="space-y-2 pl-2">
-                  {docsByFolder[folder].map((doc) => renderDocLink(doc, folder))}
+                  {docsByFolder[folder].map((doc) =>
+                    renderDocLink(doc, folder)
+                  )}
                 </ul>
               )}
             </div>
