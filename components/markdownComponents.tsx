36619d06ae9b6fd2b94e47c2e0f10605d0db8368
unknown <iverfinne@gmail.com>
2023-06-26 07:58:43 +0200

    minor fix

--- components/markdownComponents.tsx
+++ components/markdownComponents.tsx
@@ -27,8 +27,9 @@ const components = {
   tr: (props) => <tr {...props} />,
   th: (props) => <th style={{ border: "1px solid #dfe2e5", padding: "6px 13px" }} {...props} />,
   td: (props) => <td style={{ border: "1px solid #dfe2e5", padding: "6px 13px" }} {...props} />,
-  blockquote: ({ children }) => <blockquote style={{ color: '#6a737d', borderLeft: '0.25em solid #dfe2e5', padding: '0 1em' }} children={children} />,
-  img: ({ node, ...props }) => <img {...props} />,
+  blockquote: ({ children }) => <blockquote style={{ color: '#6a737d', borderLeft: '0.25em solid #dfe2e5', padding: '0 1em' }}>{children}</blockquote>,
+  hr: () => <hr />,
+  img: ({ alt = '', src, ...props }) => <img alt={alt} src={src} className={`markdown-img ${props.className}`} {...props} />
 }
 
 export default components;
